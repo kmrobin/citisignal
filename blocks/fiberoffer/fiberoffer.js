03d091fdfd6a5988247b412e28b2c61de461ffc7
Robin KM <robinsanghi@hotmail.com>
2025-05-08 15:53:09 +0530

    Update fiberoffer.js

--- blocks/fiberoffer/fiberoffer.js
+++ blocks/fiberoffer/fiberoffer.js
@@ -9,4 +9,22 @@ export default function decorate(block) {
   offerCTA.className = 'offerCTA';
   offerImage.id = 'offerImage';
   offerImage.className = 'offerImage';
+
+  var script1 = document.createElement('script');
+  script1.text = "!function(n,o){o.forEach(function(o){n[o]||((n.__alloyNS=n.__alloyNS||[]).push(o),n[o]=function(){var u=arguments;return new Promise(function(i,l){n[o].q.push([i,l,u])})},n[o].q=[])})}(window,['alloy']);"
+  document.head.appendChild(script1);
+
+  var script2 = document.createElement('script');
+  script2.async = true;
+  script2.src = "https://cdn1.adoberesources.net/alloy/2.14.0/alloy.min.js";
+  document.head.appendChild(script2);
+
+  alloy("configure", {
+    "edgeConfigId": "045c5ee9-468f-47d5-ae9b-a29788f5948f",
+    "orgId": "907075E95BF479EC0A495C73@AdobeOrg",
+    "defaultConsent": "in"
+  });
+
 }
+
+
